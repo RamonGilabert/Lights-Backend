2bfaff03bb0281a194edd3eac79bdab380aff0bb
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-23 21:57:14 +0100

    Adds information

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var pg = require('pg');
 var app = express();
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432';
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,7 +16,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-pg.connect(connectionString + '?ssl=true', function(error, client) {
+pg.connect(process.env.DATABASE_URL, function(error, client) {
   if (error) throw error;
 
   console.log('Connected to postgres! Getting schemas...');
