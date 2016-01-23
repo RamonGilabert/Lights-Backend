903bd082533731e4e028f22715d72686cbb17be3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-23 21:04:48 +0100

    Adds a better code

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.json({ message: 'Hello! Welcome to Lights.' });
 });
 
 app.listen(app.get('port'), function() {
