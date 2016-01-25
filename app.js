d1ff7bb7ea2c293e4dd77a3aa98301bd6a304cf4
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-25 09:14:03 +0100

    Adds another fourth implementation

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var pg = require('pg');
 var app = express();
 var databaseAddress = process.env.DATABASE_URL || 'postgres://localhost';
 var bookshelf = require('./app/database/schema.js')(databaseAddress);
 
-require('./app/routes/lights.js')(app, bookshelf);
-require('./app/routes/controllers.js')(app, bookshelf);
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.set('bookshelf', bookshelf);
@@ -16,4 +15,7 @@ app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+
+  require('./app/routes/lights.js')(app, bookshelf);
+  require('./app/routes/controllers.js')(app, bookshelf);
 });
--- app/routes/lights.js
+++ app/routes/lights.js
@@ -21,9 +21,26 @@ module.exports = function(app, bookshelf) {
   /* PUT */
 
   app.put('/lights/:id', function(request, response) {
-    console.log(request['params']['id']);
-    new Light({ 'id' : request['params']['id'] }).fetch().then(function(lights) {
-      response.json({ message: 'Cool story!', light: lights });
+    new Light({ 'id' : request['params']['id'] }).fetch().then(function(light) {
+      var body = request['body'];
+
+      if (body === undefined || body['id'] === undefined || body['controller_id'] === undefined
+      || body['status'] === undefined || body['intensity'] === undefined
+      || body['red'] === undefined || body['green'] === undefined || body['blue'] === undefined) {
+        response.sendStatus(400);
+      } else {
+        light['id'] = body['id'];
+        light['controller_id'] = body['controller_id'];
+        light['status'] = body['status'];
+        light['intensity'] = body['intensity'];
+        light['red'] = body['red'];
+        light['blue'] = body['blue'];
+        light['green'] = body['green'];
+
+        console.log(light);
+
+        response.json({ message: 'Cool story!', light: light });
+      }
     });
   });
 
