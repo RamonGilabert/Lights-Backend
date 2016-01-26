746eda9953e4e47da4cd57bc0b2c4027b99aff48
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-26 22:25:21 +0100

    Adding the fetching of one

--- app/routes/controllers.js
+++ app/routes/controllers.js
@@ -9,7 +9,17 @@ module.exports = function(app, bookshelf) {
   app.get('/controllers', function(request, response) {
     new Controllers().fetchAll().then(function(controllers) {
       response.json(controllers.toJSON());
+    }).catch(function(error) {
+      response.sendStatus(500);
     });
+  });
+
+  app.get('controllers/:id', function(request, response) {
+    new Controllers({ 'id' : request['headers']['controller_id'] }).fetch().then(function(controller) {
+      response.json({ message : 'Cool story!', controller : controller })
+    }).catch(function(error) {
+      response.sendStatus(500);
+    })
   });
 
   /* POST */
