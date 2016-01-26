df1683a9798316f269747a73d564a06fa3908a6e
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-26 22:28:56 +0100

    You can get the controllers now

--- app/routes/controllers.js
+++ app/routes/controllers.js
@@ -14,12 +14,16 @@ module.exports = function(app, bookshelf) {
     });
   });
 
-  app.get('controllers/:id', function(request, response) {
-    new Controllers({ 'id' : request['headers']['controller_id'] }).fetch().then(function(controller) {
-      response.json({ message : 'Cool story!', controller : controller })
-    }).catch(function(error) {
-      response.sendStatus(500);
-    })
+  app.get('/controllers/:id', function(request, response) {
+    if (parseInt(request['params']['id']) === parseInt(request['headers']['controller_id'])) {
+      new Controllers({ 'id' : request['params']['id'] }).fetch().then(function(controller) {
+        response.json({ message : 'Cool story!', controller : controller })
+      }).catch(function(error) {
+        response.sendStatus(500);
+      });
+    } else {
+      response.sendStatus(400);
+    }
   });
 
   /* POST */
