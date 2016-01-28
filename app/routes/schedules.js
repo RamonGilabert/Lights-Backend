85498dc90f91f6afb994364fb7139907c348cdbf
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-28 22:39:41 +0100

    Adding more stuff

--- app/routes/schedules.js
+++ app/routes/schedules.js
@@ -2,9 +2,15 @@
 
 module.exports = function(app, bookshelf) {
 
+  var Schedyle = require('../models/schedules.js')(bookshelf);
+
+  // GET
+
   app.get('/schedule', function(request, response) {
     // TODO: Show the schedules for that time.
   });
+
+  // POST
 
   app.post('/schedule/:id', function(request, response) {
     new Light({ 'id' : request['headers']['controller_id'] }).fetch().then(function(light) {
