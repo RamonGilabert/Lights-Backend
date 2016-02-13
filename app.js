60ad58579a8df810c5f6144a171d52ff1d0fd084
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-02-13 21:27:05 +0100

    Adds the socket

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var pg = require('pg');
 var app = express();
 var databaseAddress = process.env.DATABASE_URL || 'postgres://localhost';
 var bookshelf = require('./app/database/schema.js')(databaseAddress);
-var io = require('socket.io')();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -14,10 +13,19 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 
   require('./app/routes/lights.js')(app, bookshelf);
   require('./app/routes/controllers.js')(app, bookshelf);
   require('./app/routes/schedules.js')(app, bookshelf);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  console.log('Connected');
+  socket.on('lights', function() {
+    socket.emit('Editing a light');
+  });
 });
